a94f39daf294b48f9d21bcc0176125fe7da6424b
unknown <rantonioivann@gmail.com>
2024-05-02 18:44:24 -0600

    DiV2

--- MainDivision.java
+++ MainDivision.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class MainDivision {
     public static void main(String[] args) {
-        Scanner d1 = new Scanner(System.in);
+        Scanner di = new Scanner(System.in);
         System.out.println("Dame el primer valor: ");
-        int n1 = d1.nextInt();
+        int n1 = di.nextInt();
         System.out.println("Dame el segundo valor: ");
-        int n2 = d1.nextInt();
-        Division d2 = new Division();
-        d2.n1 = n1;
-        d2.n2 = n2;
-        System.out.println("El resultado es: " + d2.mostrarResultado());
+        int n2 = di.nextInt();
 
+        Division d1 = new Division();
+        d1.setN1(n1);
+        d1.setN2(n2);
+        d1.mostrarResultado();
+        System.out.println("El resultado de la division es: " + d1.mostrarResultado());
+        System.out.println(d1.toString());
     }
 }
