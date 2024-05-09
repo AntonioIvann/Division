b70f04857cfc4a01b7c135ea5fedd4ea181c69bb
unknown <rantonioivann@gmail.com>
2024-05-08 21:31:53 -0600

    v3

--- Division.java
+++ Division.java
@@ -1,37 +1,42 @@
-public class Division {
-    private int n1, n2, res;
+public class Division { // Declara una clase pública llamada Division
+    private int n1, n2, res; // Declara tres variables enteras privadas: n1, n2 y res (resultado)
 
+    // Método para realizar la división y devolver el resultado
     public int mostrarResultado() {
-        res = n1 / n2;
-        return res;
+        res = n1 / n2; // Calcula la división de n1 entre n2 y almacena el resultado en res
+        return res; // Devuelve el resultado de la división
     }
 
+    // Métodos getter y setter para n1
     public int getN1() {
-        return n1;
+        return n1; // Devuelve el valor de n1
     }
 
     public void setN1(int n1) {
-        this.n1 = n1;
+        this.n1 = n1; // Establece el valor de n1 utilizando el valor proporcionado
     }
 
+    // Métodos getter y setter para n2
     public int getN2() {
-        return n2;
+        return n2; // Devuelve el valor de n2
     }
 
     public void setN2(int n2) {
-        this.n2 = n2;
+        this.n2 = n2; // Establece el valor de n2 utilizando el valor proporcionado
     }
 
+    // Métodos getter y setter para res (resultado)
     public int getRes() {
-        return res;
+        return res; // Devuelve el resultado almacenado
     }
 
     public void setRes(int res) {
-        this.res = res;
+        this.res = res; // Establece el resultado utilizando el valor proporcionado
     }
 
+    // Método toString para representar el objeto en forma de cadena
     @Override
     public String toString() {
-        return "Suma [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]";
+        return "Division [n1=" + n1 + ", n2=" + n2 + ", res=" + res + "]"; // Devuelve una cadena representando el objeto
     }
 }
--- MainDivision.java
+++ MainDivision.java
@@ -1,18 +1,26 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
 
-public class MainDivision {
-    public static void main(String[] args) {
-        Scanner di = new Scanner(System.in);
-        System.out.println("Dame el primer valor: ");
-        int n1 = di.nextInt();
-        System.out.println("Dame el segundo valor: ");
-        int n2 = di.nextInt();
+public class MainDivision { // Declara una clase pública llamada MainDivision
+    public static void main(String[] args) { // Método principal, punto de entrada del programa
+        Scanner di = new Scanner(System.in); // Crea un objeto Scanner llamado di para leer la entrada del usuario
 
-        Division d1 = new Division();
+        System.out.println("Dame el primer valor: "); // Solicita al usuario que ingrese el primer valor
+        int n1 = di.nextInt(); // Lee el primer valor proporcionado por el usuario
+
+        System.out.println("Dame el segundo valor: "); // Solicita al usuario que ingrese el segundo valor
+        int n2 = di.nextInt(); // Lee el segundo valor proporcionado por el usuario
+
+        Division d1 = new Division(); // Crea un objeto de la clase Division llamado d1
+
+        // Asigna los valores proporcionados por el usuario al objeto d1
         d1.setN1(n1);
         d1.setN2(n2);
+
+        // Muestra el resultado de la división utilizando el método mostrarResultado() de d1
         d1.mostrarResultado();
         System.out.println("El resultado de la division es: " + d1.mostrarResultado());
+
+        // Muestra una representación textual del objeto d1 utilizando el método toString()
         System.out.println(d1.toString());
     }
 }
